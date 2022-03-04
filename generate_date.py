2a6e55d18c6018be4f606dbbe87ad78a6e110692
Marcin <gonzur75@github.com>
2022-03-04 19:31:33 +0100

    generator

--- generate_date.py
+++ generate_date.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+import json
 
 from app import Event
 
@@ -8,24 +9,38 @@ class GenerateData:
         self.title = ['piwo', 'wino', 'wódkę']
         self.location = ['berlin', 'tokyo', 'donbas']
         self.duration = randint(15, 599)
-        self.users = ['ala', 'ola', 'ela']
+        self.users = ['ala', 'ola', 'ela', 'ula', 'roman']
+
+        self.__dupa = 'private - name mangling' # nie uzywać
+        self.__dupa = 'protected' # używamy tylko w funkcji
+        self.dupa = 'public'
+        self.DUPA1 = 'read_only'
 
     @staticmethod
     def _start_time_generator():
         return f'{randint(1, 28)}-{randint(3, 12)}-{randint(2022, 2050)} {randint(1, 23)}:{randint(0, 59)}'
 
-    def do_magic(self, amount):
+
+    def generate(self, amount, path):
         temp = []
         for i in range(amount):
-            temp.append(Event(
-                choice(self.title),
-                choice(self.location),
-                self._start_time_generator(),
-                randint(15, 600),
-                choice(self.users),
-                [choice(self.users) for _ in range(randint(0, (len(self.users) - 1)))]
-            ))
-        print(temp)
+            temp.append({
+                'title': choice(self.title),
+                'location': choice(self.location),
+                'start_time': str(self._start_time_generator()),
+                'duration': randint(15, 600),
+                'owner': choice(self.users),
+                'participants': [choice(self.users) for _ in range(randint(1, (len(self.users) - 1)))]
+            })
 
-g = GenerateData()
-g.do_magic(100)
+        with open(path, 'w', encoding='UTF-8') as file:
+            json.dump(temp, file)
+
+    @staticmethod
+    def load(path):
+        with open(path, 'r') as file:
+            return json.load(file)
+
+    def __repr__(self):
+        attr = ', '.join(f'{k if k[0] != "_" else k[1:]}={v}' for k, v in self.__dict__.items())
+        return f"{type(self).__name__}({attr})"
